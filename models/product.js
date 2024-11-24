2af74314433825336275ea883bac0b91fc631f90
AbdulmoizAbdulmaged <149574102+AbdulmoizAbdulmaged@users.noreply.github.com>
2024-11-24 20:45:36 +0300

    deploy discount

--- models/product.js
+++ models/product.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose');
 const ProductSchema = new mongoose.Schema(
   {
+    barcode: {type: String, required: true, unique: true},
     title: {type: String, required: true,unique: true},
     desc: {type: String,required: true},
     img: {type: String,required: true},
+    season: {type: String, required: true},
     categories: {type: Array},
     size: {type: Array},
     color: {type: Array},
@@ -11,6 +13,7 @@ const ProductSchema = new mongoose.Schema(
     selectedSize:{type: String, default:'none'},
     price: {type: Number, required: true},
     inStock: {type: Number, required:true},
+    discount: {type: Number, default: 0},
     
     
 },
