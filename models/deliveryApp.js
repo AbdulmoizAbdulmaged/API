46aad50e813241c33a17640ea195658c67bbe322
AbdulmoizAbdulmaged <149574102+AbdulmoizAbdulmaged@users.noreply.github.com>
2025-10-10 22:11:32 +0300

    update app setting to enter max dist,bank account,etc..

--- models/deliveryApp.js
+++ models/deliveryApp.js
@@ -11,6 +11,20 @@ const DeliveryAppSchema = new mongoose.Schema({
   end: {type: String, default: '10:00 PM'},
   active: { type: Boolean, default: true },
   ads: { type: String, default: '' },
+  max_distance: {type:Number, default: 5000}, // in meters
+  min_order_price: {type:Number, default: 5000},
+  max_cash_value: {type:Number,default: 10000},
+  bank_account_number: {type:String, default:''},
+  facebook: {type:String, default:''},
+  instagram: {type:String, default:''},
+  twitter: {type:String, default:''},
+  tiktok: {type:String, default:''},
+  youtube: {type:String, default:''},
+  whatsapp: {type:String, default:''},
+  phone: {type:String, default:''},
+  email: {type:String, default:''},
+  address: {type:String, default:''},
+  
 }, 
 { timestamps: true });
 
